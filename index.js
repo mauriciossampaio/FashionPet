97e99efe4f61b03acc38173e3f38acff786b68e1
mauriciossampaio <141499529+mauriciossampaio@users.noreply.github.com>
2024-06-14 19:22:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config();
-
 const app = express();
 
-// Configuração do Body-Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Configuração do EJS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-// Conexão ao MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -23,14 +15,11 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB conectado...'))
 .catch(err => console.log(err));
 
-// Rotas
 const userRoutes = require('./src/routes/userRoutes');
 const productRoutes = require('./src/routes/productRoutes');
-
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 
-// Porta de escuta do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
